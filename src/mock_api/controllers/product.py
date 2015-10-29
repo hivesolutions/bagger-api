7b7e46dfaa25d57444aa8d822d04819e1a3398ef
Afonso Caldas <acaldas@hive.pt>
2015-10-29 14:17:28 +0000

    product controller refactoring

--- src/mock_api/controllers/product.py
+++ src/mock_api/controllers/product.py
@@ -7,15 +7,11 @@ import mock_api
 
 class ProductController(appier.Controller):
 
-    @appier.route("/products/<int:id>.json", "GET")
+    @appier.route("/products/<int:id>", "GET")
     def show(self, id):
         product = mock_api.Product.get(id = id, map = True)
         return product
 
     @appier.route("/products/<int:id>.png", "GET")
     def image(self, id):
-        path = "resources/images/product%s.png" % id
-        file = open(path, "rb")
-        try: data = file.read()
-        finally: file.close()
-        return data
+        return self.send_static("images/product%d.png" % id)
